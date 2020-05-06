7068a2b1be87d8632f029e77fb9931b60eb0cee4
Anubhav Vats <anubhav.cse1999@gmail.com>
2020-05-07 00:59:10 +0530

    example updated

--- example.cpp
+++ example.cpp
@@ -10,10 +10,11 @@ using namespace GF256;
 using namespace shamir;
 
 int main(int argc,char** argv) {
-  init();
-  scheme scheme1(125,124);
-  shares* scheme1_shares = scheme1.createShares("anubhav vats");
-  for(int i=0;i<4;i++) {
+  init();//to initialise the library functions.Must be used to use the library.
+  scheme scheme1(8,5); //creating a scheme with 8 shares and 5 threshold.
+  shares* scheme1_shares = scheme1.createShares("its our team secret");
+  //printing shares of all the participants...
+  for(int i=0;i<8;i++) {
       cout << "shares of pariticipant " << (i+1) << " => " << endl;
       for(auto val:(*scheme1_shares)[i]) {
           cout << val.x << " " << val.y << endl;
@@ -21,7 +22,19 @@ int main(int argc,char** argv) {
       cout <<  "_____________________________________________________" << endl;
 
   }
+  //reconstructing the secret by giving  shares of all 8 members...
   string secret = scheme1.getSecret(scheme1_shares);
-  cout << secret << endl;
+  cout << "construted secret using 8 shares -> " <<secret << endl;
+
+  //reconstructing the secret by only shares of 5 members...
+  shares* shares5 = new shares(5);
+  for(int i=0;i<5;i++) {
+    for(auto val:(*scheme1_shares)[i]) {
+        (*shares5)[i].push_back(val);
+    }
+  }
+  string newSecret = scheme1.getSecret(shares5);
+  cout << "construted secret using 5 shares -> " << newSecret << endl;
+
   return 0;
 }
